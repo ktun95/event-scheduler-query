7a0d556df15f07952236f364a5337592af2b872e
Kevin Tun <kevin.tun@northwestern.edu>
2025-11-13 12:13:43 -0600

    fix(get_median.ts): use defined `Interval` type

--- core/get_median.ts
+++ core/get_median.ts
@@ -1,12 +1,14 @@
+import { type Interval } from "./types"
 import { bubble_sort } from "./sorting/bubble_sort"
 
-function get_median(intervals: Array<[number, number]>): number {
+function get_median(intervals: Array<Interval>): number {
 	const points = []
-	for (const interval of intervals) {
-		points.push(interval[0])
-		points.push(interval[1])
+	for (let i = 0; i < intervals.length; i++) {
+		points.push(intervals[i][0])
+		points.push(intervals[i][1])
 	}
 	bubble_sort(points)
+	console.log(points)
 	const median_index = Math.floor(points.length / 2)
 	return points[median_index]
 }
