d0779f784626b1beec22a9f942f69d66fa14490f
Kevin Tun <kevin.tun@northwestern.edu>
2025-11-14 08:48:38 -0600

    feat(get_median.test.ts): change interval sets to arrays for get_median tests
    The test now uses arrays instead of sets, which aligns with the get_median function's expected input type. This change ensures that the tests accurately reflect the function's usage and prevents potential type-related issues.

--- tests/get_median.test.ts
+++ tests/get_median.test.ts
@@ -2,13 +2,13 @@ import { test, expect } from "vitest"
 import { get_median } from "@/core/get_median"
 
 test("Get median", () => {
-	const test_interval_set_one: Set<[number, number]> = new Set([[1, 2], [3, 4], [5, 6]])
+	const test_intervals_one: Array<[number, number]> = ([[1, 2], [3, 4], [5, 6]])
 	const expected_one = 4
-	const test_interval_set_two: Set<[number, number]> = new Set([[7, 14], [1, 8], [2, 9]])
+	const test_intervals_two: Array<[number, number]> = ([[7, 14], [1, 8], [2, 9]])
 	const expected_two = 8
 
-	const median_one = get_median(test_interval_set_one)
-	const median_two = get_median(test_interval_set_two)
+	const median_one = get_median(test_intervals_one)
+	const median_two = get_median(test_intervals_two)
 
 	expect(median_one).toEqual(expected_one)
 	expect(median_two).toEqual(expected_two)
